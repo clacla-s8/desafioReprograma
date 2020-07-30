a0158e69f69fa4ff2a4775700dc28a29996bec01
clacla-s8 <clariceks8@gmail.com>
2020-07-29 22:22:05 -0300

    insercao de tratamento de valores menores ou igual a zero e troca de ' . ' por ' , '

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ var resultado = document.querySelector("#resultado");
     resultado.textContent = "R$" + (valorProjeto).toFixed(2);
 } */
 
+// insercao de tratamento de valores menores ou igual a zero e troca de ' . ' por ' , ' 
 function calcular() {
     var valorProjeto = (valorHora.valueAsNumber * horasProjeto.valueAsNumber);
     if (Number.isNaN(valorProjeto)) {
         alert("Preencha todos os campos !");
+    } else if (valorHora.valueAsNumber <= 0 || horasProjeto.valueAsNumber <= 0) {
+        alert("Insira um valor maior que 0");
     } else {
-        resultado.textContent = "R$" + (valorProjeto).toFixed(2);
+        resultado.textContent = "R$" + (valorProjeto.toString().replace(".", ","));
     }
 }
